7b91d14fe7c896cee9dc49be94ef37480b47a55d
Iustin2131 <126079873+Iustin2131@users.noreply.github.com>
2024-06-25 05:53:48 +0300

    ex

--- export.php
+++ export.php
@@ -12,7 +12,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
     $sql = "
         SELECT 
-            name_shoes, category, brand, price, description, image, link, rating,
+            name_shoes, category, brand, price, description, image, link,
             red, yellow, green, blue, black, white,
             size_33, size_34, size_35, size_36, size_37, size_38, size_39, size_40, size_41, size_42, size_43, size_44, size_45
         FROM 
